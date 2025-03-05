b8bad8451a485ed64a017d45324f7f7cfa3d437a
Khushi Goyal <147341925+khushi1k4@users.noreply.github.com>
2025-03-05 21:32:17 +0000

    dataTypes added

---
+++ Basic/dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all js code as new version
+
+// alert(3 + 3) //js engine is hidden inside the browser. so mostly things run in it. pop up window shown when we write it in console of browser
+// we are using nodejs, not browser
+
+// console.log(3+3) console.log("hitesh") //error will be occured
+
+// console.log(3+3); console.log("hitesh") //it will run but not clean way of writting
+
+// console.log(3+3) 
+// console.log("hitesh")
+
+let name = "Khushi" //string
+let age = 18 //int
+let isLoggedIn = true
+
+// number => 2 to power 53
+// bigint
+// boolean => true/false
+// null => standalone value
+// undefined => no value assigned to it
+// symbol => for the uniqueness
+
+// object 
+
+console.log(typeof undefined); // undefined is type of undefined itself
+console.log(typeof null) // null is type of object
