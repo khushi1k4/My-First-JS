d3937c2c7c0e18ac3569a14c06be713b6eb8f7ff
Khushi Goyal <147341925+khushi1k4@users.noreply.github.com>
2025-03-08 18:02:34 +0000

    conversionOf_dataTypes

---
+++ Basic/conversionOperation_datatypes.js
@@ -0,0 +1,86 @@
+// let score = "33"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+
+//**conversion of dataTypes**
+// let valueInNumber = Number(score) //dataTypes first letter must be capital/in Upper case for dataType's conversion
+// let valueInNumber = score 
+
+// console.log(typeof valueInNumber);
+
+// let marks;
+// console.log(typeof (marks)); //undefined is it's dataType
+
+// let score = "ab";
+// let Value = Number(score)
+// console.log(Value); //NaN refers "not a number" for the non numeric values in it if wrong dataTypes is mentioned during the dataTypes conversion
+
+// let value = "abc";
+// let hell = String(value)
+// console.log(hell); //abc is it's output
+
+// let grant = null;
+// console.log(grant); //null is it's output
+// let giveValue = Number(grant);
+// console.log(giveValue); //0 is it's output
+
+// let grant = undefined;
+// console.log(grant); //undefined is it's output
+// let giveValue = Number(grant);
+// console.log(giveValue); //NaN is it's output
+
+// let raise = ""; //0 is the output
+// let loggedIn = Number(raise)
+// console.log(loggedIn);
+
+// let raise1 = ""; //0 is the output
+// let loggedIn1 = Number(raise1)
+// console.log(loggedIn1);
+
+// let mark = true;
+// let result = Number(mark);
+// console.log(result);
+
+// let mark2 = false;
+// let result =Number(mark2);
+// console.log(result);
+
+// **SUMMARY OF DATAtype CONVERSIONS OF STRINGS**
+// "33" => Number => 33
+// "33abc" => Number => NaN(Not a Number)
+// "abc" => Number => NaN
+// true => Number => 1
+// false => Number => 0 
+// "" => Number => 0
+// " " => Number => 0
+
+// let raise = ""; // false is the output
+// let loggedIn = Boolean(raise)
+// console.log(loggedIn);
+
+// let raise = "4"; //true is the output
+// let loggedIn =Boolean(raise)
+// console.log(loggedIn);
+
+// let raise1 = " "; //true is the output
+// let loggedIn1 = Boolean(raise1)
+// console.log(loggedIn1);
+
+// let raise = "abc"; //true is the output
+// let loggedIn =Boolean(raise)
+// console.log(loggedIn);
+
+// **SUMMARY OF DATAtype CONVERSIONS OF BOOLEAN**
+// 0 => false ; 1 => true
+// "" => false
+// " " => Boolean => true
+// "abc" => Boolean => true
+// "4" => Boolean => true
+
+let someNumber = 30 
+let stringNumber= String(someNumber) //Number converted into string
+console.log(stringNumber);
+console.log(typeof(stringNumber));
+
